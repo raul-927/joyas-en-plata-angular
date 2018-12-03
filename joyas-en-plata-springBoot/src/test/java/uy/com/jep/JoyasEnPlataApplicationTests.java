cf5e36e849aac68ab8705bbc47149939652f8bbc
raraherher <raraherher@raraherher-System-Product-Name>
2018-12-03 12:03:26 -0300

    Inicio

---
+++ joyas-en-plata-springBoot/src/test/java/uy/com/jep/JoyasEnPlataApplicationTests.java
@@ -0,0 +1,16 @@
+package uy.com.jep;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JoyasEnPlataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
