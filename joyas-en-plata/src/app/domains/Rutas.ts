c1ceedf464e71f2cbf2d5b3cc687ad2df9ca1a23
raraherher <raraherher@raraherher-System-Product-Name>
2019-01-15 12:07:44 -0300

    Rutas.ts

---
+++ joyas-en-plata/src/app/domains/Rutas.ts
@@ -0,0 +1,29 @@
+import {
+RouterModule,
+Routes
+} from '@angular/router';
+import { ClientResponse } from 'http';
+import {ClienteComponent} from '../components/cliente/cliente.component';
+
+// const routes: Routes = [
+// basic routes
+// { path: '', redirectTo: 'home', pathMatch: 'full' },
+// { path: 'home', component: HomeComponent },
+// { path: 'about', component: AboutComponent },
+// { path: 'contact', component: ContactComponent },
+// { path: 'contactus', redirectTo: 'contact' },
+    // {path: 'clientes', component: ClienteComponent},
+// authentication demo
+// { path: 'login', component: LoginComponent },
+// {
+// path: 'protected',
+// component: ProtectedComponent,
+// canActivate: [ LoggedInGuard ]
+// },
+// // nested
+// {
+// path: 'products',
+// component: ProductsComponent,
+// children: childRoutes
+// }
+// ];
